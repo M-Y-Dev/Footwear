de8d731e3556c028acb3ae678ca7fbe053c217a9
Gulsah <gulsharslan@gmail.com>
2024-07-30 20:51:03 +0300

    basket and category entity added

---
+++ Core/Footwear.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using Footwear.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footwear.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string CategoryName { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
