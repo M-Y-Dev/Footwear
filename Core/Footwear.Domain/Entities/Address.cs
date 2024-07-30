c9f468653639789728c61736ae0c873d3d782997
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-07-30 20:55:21 +0300

    Address&Comment Entityleri oluşturuldu.

---
+++ Core/Footwear.Domain/Entities/Address.cs
@@ -0,0 +1,17 @@
+﻿using Footwear.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footwear.Domain.Entities
+{
+    public class Address:BaseEntity
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string AddressDetail { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Core/Footwear.Domain/Entities/Common/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footwear.Domain.Entities.Common
+{
+    public class Comment:BaseEntity
+    {
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string ProductId { get; set; }
+        public string CommentDetail { get; set; }
+        public bool Status { get; set; }
+    }
+}
