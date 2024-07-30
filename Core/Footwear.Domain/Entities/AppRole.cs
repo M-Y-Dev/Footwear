10ff38957a151b829569b82443484e0da45a89aa
gozgirfaruk <gozgirfaruk@gmail.com>
2024-07-30 18:05:05 +0300

    User&Role Entityleri gerçekleştirildi.

---
+++ Core/Footwear.Domain/Entities/AppRole.cs
@@ -0,0 +1,16 @@
+﻿using Footwear.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footwear.Domain.Entities
+{
+    public class AppRole : BaseEntity
+    {
+        public string RoleName { get; set; }
+        public List<AppUser> AppUsers { get; set; }
+    }
+
+}
---
+++ Core/Footwear.Domain/Entities/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using Footwear.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footwear.Domain.Entities
+{
+    public class AppUser : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ImageUrl { get; set; }
+        public int RoleId { get; set; }
+        public AppRole AppRole { get; set; }
+        public string Password { get; set; }
+        public string ConfirmedPassword { get; set; }
+    }
+}
