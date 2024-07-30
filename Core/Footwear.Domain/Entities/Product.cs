b8ea4e17371faafe95ffd8943c850037483aec62
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-07-30 19:19:44 +0200

    Product and ProductDetail Entities Created

---
+++ Core/Footwear.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using Footwear.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footwear.Domain.Entities
+{
+    public class AppRole : BaseEntity
+    {
+        public string ProductName { get; set; }
+        public string ProductImageUrl { get; set; }
+        public int ProductStock { get; set; }
+        public decimal Price { get; set; }
+        public  int CategoryID { get; set; }
+        public List<ProductDetail> ProductDetails { get; set; }
+    }
+
+}
