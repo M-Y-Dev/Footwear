ec80142ab5ec98b5f387ffa1e0634798e46fbd3c
Ensar Sarac <76907308+ensarsarac@users.noreply.github.com>
2024-07-31 19:36:12 +0300

    request response example

---
+++ Core/Footwear.Application/Mediator/Commands/TestCommands/TestCommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using Footwear.Application.Base;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footwear.Application.Mediator.Commands.TestCommands
+{
+    public class TestCommandRequest:IRequest<Response<TestCommandResponse>>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Core/Footwear.Application/Mediator/Commands/TestCommands/TestCommandResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footwear.Application.Mediator.Commands.TestCommands
+{
+    public class TestCommandResponse
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
