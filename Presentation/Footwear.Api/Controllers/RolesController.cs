7d8da7aadfa0d25647f0ab42a90b351872dd39d5
gozgirfaruk <gozgirfaruk@gmail.com>
2024-08-01 22:08:39 +0300

    User&Role Endpoint tamamlandı.

---
+++ Presentation/Footwear.Api/Controllers/RolesController.cs
@@ -0,0 +1,51 @@
+﻿using Footwear.Application.Mediator.Commands.RoleCommands;
+using Footwear.Application.Mediator.Queries.RoleQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Footwear.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RolesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRoleList()
+        {
+            var values = await _mediator.Send(new GetRoleQuery());
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRoleById(int id)
+        {
+            var values = await _mediator.Send(new GetRoleByIdQuery(id));
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateRole(UpdateRoleCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            await _mediator.Send(new DeleteRoleCommand(id));
+            return Ok();
+        }
+    }
+}
---
+++ Presentation/Footwear.Api/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Footwear.Application.Mediator.Commands.UserCommands;
+using Footwear.Application.Mediator.Queries.UserQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Footwear.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserList()
+        {
+            var values = await _mediator.Send(new GetUserQuery());
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var values = await _mediator.Send(new GetUserByIdQuery(id));
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser(UpdateUserCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var values = await _mediator.Send(new DeleteUserCommand(id));
+            return Ok();
+
+        }
+    }
+}
