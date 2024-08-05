b692bd597fac7332ce6c2a083547e5ee1f19cc09
Ensar Sarac <76907308+ensarsarac@users.noreply.github.com>
2024-08-05 23:54:07 +0300

    note

--- Presentation/Footwear.UI/Areas/Admin/Controllers/ProductController.cs
+++ Presentation/Footwear.UI/Areas/Admin/Controllers/ProductController.cs
@@ -22,6 +22,9 @@ namespace Footwear.UI.Areas.Admin.Controllers
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7275/api/products/GetProductWithCategory");
+
+            //response message base adresden sonra değişecek.
+
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             var jsonObject = JsonConvert.DeserializeObject<dynamic>(jsonData);
             
