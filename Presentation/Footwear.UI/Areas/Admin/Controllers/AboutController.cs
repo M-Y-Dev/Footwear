9b11d960e78ea2302f00dbe8e3d9f2cf94595ee7
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-08-07 16:06:12 +0300

    about,admin panelinde consume edilip, crud işlemleri yapıldı

---
+++ Presentation/Footwear.UI/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,89 @@
+﻿using Footwear.UI.Areas.Admin.Dtos.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Footwear.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task <IActionResult> AboutList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7275/api/abouts");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var jsonObject = JsonConvert.DeserializeObject<dynamic>(jsonData);
+
+            if((bool)jsonObject.responseIsSuccessfull)
+            {
+                var result = jsonObject.responseData;
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(result.ToString());
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7275/api/abouts/{id}");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("AboutList");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateAbout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(CreateAboutDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7275/api/abouts", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("AboutList");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7275/api/abouts/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7275/api/abouts",stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("AboutList");
+            }
+            return View();
+        }
+    }
+}
