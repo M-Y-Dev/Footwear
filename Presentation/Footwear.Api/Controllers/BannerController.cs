93a6bde55892bb6977a10227e331e9f92144a330
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-08-22 16:05:19 +0300

    brand ve banner api controllerları yazıldı

---
+++ Presentation/Footwear.Api/Controllers/BannerController.cs
@@ -0,0 +1,53 @@
+﻿using Footwear.Application.Mediator.Commands.BannerCommands;
+using Footwear.Application.Mediator.Queries.BannerQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Footwear.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BannerController ( IMediator mediator )
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBanner ( CreateBannerCommand model )
+        {
+            var result = await _mediator.Send(model);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBanner ( int id )
+        {
+            var result = await _mediator.Send(new DeleteBannerCommand(id));
+            return Ok(result);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateBanner ( UpdateBannerCommand model )
+        {
+            var result = await _mediator.Send(model);
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBannerById ( int id )
+        {
+            var result = await _mediator.Send(new GetBannerByIdQuery(id));
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBanner ()
+        {
+            var result = await _mediator.Send(new GetBannerQuery());
+            return Ok(result);
+        }
+    }
+}
+
---
+++ Presentation/Footwear.Api/Controllers/BrandController.cs
@@ -0,0 +1,53 @@
+﻿using Footwear.Application.Mediator.Commands.BrandCommand;
+using Footwear.Application.Mediator.Queries.BrandQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Footwear.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BrandController ( IMediator mediator )
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBrand ( CreateBrandCommand model )
+        {
+            var result = await _mediator.Send(model);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBrand ( int id )
+        {
+            var result = await _mediator.Send(new DeleteBrandCommand(id));
+            return Ok(result);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateBrand ( UpdateBrandCommand model )
+        {
+            var result = await _mediator.Send(model);
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBrandById ( int id )
+        {
+            var result = await _mediator.Send(new GetBrandByIdQuery(id));
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBrand ()
+        {
+            var result = await _mediator.Send(new GetBrandQuery());
+            return Ok(result);
+        }
+    }
+}
+
